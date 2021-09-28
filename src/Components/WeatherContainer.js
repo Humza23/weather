192d7c815550b52a14c10a69a7f4ba81af39d490
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-09-28 18:26:24 -0400

    wip

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -20,10 +20,20 @@ const WeatherContainer = (props) => {
         <div className='display'>
             <h2> Please enter a city name </h2>
         </div>
-          : 
+          : props.weather.stateName ?
         <div className='display'>
-
             <h2> {props.weather.cityName}, {props.weather.stateName} </h2>
+            <h2> {props.weather.weather} </h2>
+            <img src={iconAPIurl} alt="weather icon" />
+            <h3> {props.weather.description}</h3>
+            <h4> {Math.round(props.weather.temperature)} </h4>
+            <p> High: {Math.round(props.weather.temperature_max)} </p>
+            <p> Low: {Math.round(props.weather.temperature_min)} </p>
+            <h5> {date} </h5>
+        </div>
+        : 
+        <div className='display'>
+            <h2> {props.weather.cityName} </h2>
             <h2> {props.weather.weather} </h2>
             <img src={iconAPIurl} alt="weather icon" />
             <h3> {props.weather.description}</h3>
