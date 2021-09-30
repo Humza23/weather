bc70ae28264a53d4e8900bcd1d4948ece7d1d043
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-09-29 22:04:33 -0400

    working time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'
+import Home from './Components/Home'
 import Weather from './Components/Weather';
 
 function App() {
   return (
     <div className="App">
+        <Home />
         <Weather />
     </div>
   );
